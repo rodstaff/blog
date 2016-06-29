9c9dc10d18f80d3b71d0cb2d9b74d637d89f77e4
rodk <rod2010rod2010@hotmail.com>
2016-06-28 21:06:06 -0400

    added updating articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     #render plain: params[:article].inspect
     #@article = Article.new(params[:article])
@@ -21,6 +25,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
